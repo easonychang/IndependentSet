5e84637d6212ead23ed90659a87f895ace0a063e
easonychang <easonchangar1997@gmail.com>
2017-05-03 17:35:55 -0700

    Initial Commit
    Basic outline

---
+++ independentSet.hpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <random>
+
+using namespace std;
+
+int main(){
+        vector <pair<int,vector<int> > > g;
+        //creating the inputGraph
+        for(int i = 0; i < 3; i++) {
+                vector<int> vec;
+                for(int j = 0; j < 2; j++) {
+                        vec[j] = rand()%2;
+                }
+                auto gpair = pair<int, vector<int> >(i,vec);
+                g[i] = gpair;
+        }
+        independentSet(g);
+
+
+
+}
+
+//Min Heap's Comp
+class pairComp {
+public:
+bool operator()(pair<int,int> lhs, pair<int,int> rhs) const {
+        return lhs.second.size() < rhs.second.size();
+}
+};
+
+vector<int> independentSet( vector<pair<int,vector<int> > > inputGraph) {
+
+        //dclaring priority queue
+        std::priority_queue<pair<int,vector<int> >, vector<pair<int,int> >, pairComp> pq;
+
+        //push every pair from inputGraph into pq
+        for(i = 0; i < inputGraph.size(); i++) {
+                pq.push(inputGraph[i]);
+        }
+
+        toExplore = pq.top();
+
+
+
+}
